28590d935b4f709ceb0ed22257b606de240a9fd4
wdfh <2545568160@qq.com>
2025-03-29 15:52:17 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    666
+    666--
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
